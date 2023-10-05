7a3651c2906cb41bdfc699213630c3fc2624d746
Lucas Acosta <70411977+Lucas-heck-acosta@users.noreply.github.com>
2023-10-05 17:57:38 -0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+    $('#telefone').mask('(00) 00000-0000')
+    $('#cpf').mask('000.000.000-00')
+    $('cep').mask('00000-000')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true,
+                minlength: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+            cep: {
+                required: true
+            },
+            endereco: {
+                required: true
+            }
+        },
+        invalidHandler: function(evento, validador)
+        {
+            let camposIncorretos = validador.numberOfInvalids()
+            console.log(camposIncorretos)
+            if (camposIncorretos > 0)
+            {
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+    })
+})
