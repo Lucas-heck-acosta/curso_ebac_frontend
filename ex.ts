728d61b5f0665b57af9240b99a4b0ddd21c42d74
Lucas Acosta <lucas.heck@live.com>
2023-10-31 16:43:39 -0300

    inital commit

---
+++ ex.ts
@@ -0,0 +1,12 @@
+function multiplica(n1:number, n2:number): number
+{
+    return n1 * n2;
+}
+
+function dizOla(nome:string): string
+{
+    return `Olá ${nome}!`;
+}
+
+multiplica(9, 10);
+dizOla("Lucas");
